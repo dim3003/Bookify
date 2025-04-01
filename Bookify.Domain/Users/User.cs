2049741db6a15e507d0e6f91304cb4f636613555
dim3003 <andre.d3003@gmail.com>
2025-04-01 09:46:21 +0200

    user entity

---
+++ Bookify.Domain/Users/User.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+public sealed class User : Entity
+{
+    public User(Guid id, Firstname firstname, LastName lastName, Email email) : 
+        base(id) 
+    { 
+        Firstname = firstname;
+        Lastname = lastName;
+        Email = email;
+    } 
+
+    public Firstname Firstname { get; set; }
+
+    public LastName Lastname { get; set; }
+
+    public Email Email { get; set; }
+
+    public static User Create(Firstname firstName, LastName lastName, Email email)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+        return user;
+    }
+}
