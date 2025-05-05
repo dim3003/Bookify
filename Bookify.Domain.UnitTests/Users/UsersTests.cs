4b3b1eb4fb98b22419b47fb68897d4f1e3de38d2
dim3003 <andre.d3003@gmail.com>
2025-05-05 09:57:50 +0200

    adding unit tests

---
+++ Bookify.Domain.UnitTests/Users/UsersTests.cs
@@ -0,0 +1,33 @@
+﻿using Bookify.Domain.Users;
+using Bookify.Domain.Users.Events;
+using FluentAssertions;
+
+namespace Bookify.Domain.UnitTests.Users;
+
+public class UsersTests
+{
+    [Fact]
+    public void Create_Should_SetPropertyValues()
+    {
+        // Act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // Assert
+        user.FirstName.Should().Be(UserData.FirstName);
+        user.LastName.Should().Be(UserData.LastName);
+        user.Email.Should().Be(UserData.Email);
+    }
+
+    [Fact]
+    public void Create_Should_RaiseUserCreatedDomainEvent()
+    {
+        // Act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // Assert
+        var domainEvent = user.GetDomainEvents().OfType<UserCreatedDomainEvent>().SingleOrDefault();
+
+        domainEvent.UserId.Should().Be(user.Id);
+    }
+
+}
