9cdb768c4ea1b39fcd91946f24f539378e0270e6
dim3003 <andre.d3003@gmail.com>
2025-05-06 16:52:07 +0200

    creating the integration tests

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -78,3 +78,5 @@ app.MapHealthChecks("health", new HealthCheckOptions
 });
 
 app.Run();
+
+public partial class Program;
---
+++ test/Bookify.Application.IntegrationTests/Infrastructure/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.Keycloak;
+using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
+
+namespace Bookify.Application.IntegrationTests.Infrastructure;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("bookify")
+        .WithUsername("postgres")
+        .WithPassword("password")
+        .Build();
+
+    private readonly RedisContainer _redisContainer = new RedisBuilder()
+        .WithImage("redis:latest")
+        .Build();
+
+    private readonly KeycloakContainer _keycloakContainer = new KeycloakBuilder()
+        .WithResourceMapping(
+            new FileInfo(".files/bookify-realm-export.json"),
+            new FileInfo("/opt/keycloak/data/import/realm.json")
+        )
+        .WithCommand("--import-realm")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestsServices(services =>
+        {
+            // Add any test-specific services here
+        });
+    }
+}
